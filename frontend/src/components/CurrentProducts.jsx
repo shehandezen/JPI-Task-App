c16589d8cc6e250fd10055ba8e821508827fdd09
shehandezen <shehandezen@gmail.com>
2024-02-05 10:31:48 +0000

    implement product search function

--- frontend/src/components/CurrentProducts.jsx
+++ frontend/src/components/CurrentProducts.jsx
@@ -10,37 +10,89 @@ import "../css/componentStyles/detailview.css";
 
 const CurrentProducts = () => {
   const search = <FontAwesomeIcon icon={faSearch} />;
-  const angle = <FontAwesomeIcon icon={faAngleRight} />;
+  // const angle = <FontAwesomeIcon icon={faAngleRight} />;
   const angles = <FontAwesomeIcon icon={faAnglesRight} />;
 
-  const [showPanel, setShowPanel] = useState(false);
+  // const [showPanel, setShowPanel] = useState(false);
 
-  const table = useRef();
-  const panel = useRef();
-  const togglePanel = () => {
-    setShowPanel(!showPanel);
-    if (showPanel) {
-      table.current.style.width = "60%";
-      panel.current.style.width = "40%";
-      panel.current.style.visibility = "visible";
-    } else {
-      table.current.style.width = "99%";
-      panel.current.style.width = "0%";
-      panel.current.style.visibility = "hidden";
-    }
-  };
+  // const table = useRef();
+  // const panel = useRef();
+  // const togglePanel = () => {
+  //   setShowPanel(!showPanel);
+  //   if (showPanel) {
+  //     table.current.style.width = "60%";
+  //     panel.current.style.width = "40%";
+  //     panel.current.style.visibility = "visible";
+  //   } else {
+  //     table.current.style.width = "99%";
+  //     panel.current.style.width = "0%";
+  //     panel.current.style.visibility = "hidden";
+  //   }
+  // };
+
+  const [data, setDate] = useState([{
+    _id: '154gfew65g1ew6v4',
+    MachineNo: 'Im 01',
+    ProductName: '4L Lid',
+    StartDate: '2024.01.25',
+    PlannedQty: 12000,
+    CurrentQty: 8000,
+    Progress: 60,
+    Efficiency: 95
+  },
+  {
+    _id: '154gfew65g1ew6v4',
+    MachineNo: 'Im 01',
+    ProductName: '250ml milk bottle',
+    StartDate: '2024.01.25',
+    PlannedQty: 12000,
+    CurrentQty: 8000,
+    Progress: 60,
+    Efficiency: 95
+  },
+  {
+    _id: '154gfew65g1ew6v4',
+    MachineNo: 'Im 01',
+    ProductName: 'morison lid',
+    StartDate: '2024.01.25',
+    PlannedQty: 12000,
+    CurrentQty: 8000,
+    Progress: 60,
+    Efficiency: 95
+  }
+    , {
+    _id: '154gfew65g1ew6v4',
+    MachineNo: 'Im 01',
+    ProductName: 'measuring cup',
+    StartDate: '2024.01.25',
+    PlannedQty: 12000,
+    CurrentQty: 8000,
+    Progress: 60,
+    Efficiency: 95
+  }
+  ])
+
+  const [searchText, setSearchText] = useState('')
+
+  const searchHandle = (e) => {
+    setSearchText(e.target.value)
+    console.log(e.target.value)
+    console.log(data.filter(obj => obj.ProductName.includes(searchText) ))
+  }
+
+
 
   return (
     <React.Fragment>
       <div className="details-container">
         <div className="top">
           <div className="search">
-            <input type="text" placeholder="Search products here" />
+            <input type="text" placeholder="Search products here" onChange={(e) => { searchHandle(e) }} />
             <div className="search-icon"> {search} </div>
           </div>
         </div>
         <div className="wrapper">
-          <div className="table-container" ref={table}>
+          <div className="table-container" >
             <table>
               <tr>
                 <th className="col-1">Machine No</th>
@@ -49,10 +101,29 @@ const CurrentProducts = () => {
                 <th className="col-4">Planned Qty</th>
                 <th className="col-5">Current Qty</th>
                 <th className="col-6">Progress</th>
-                <th className="col-7">Efficieny</th>
+                <th className="col-7">Efficiency</th>
                 <th className="col-8">{angles}</th>
               </tr>
-              <tr>
+
+              {
+                data?.filter(obj => obj?.ProductName?.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()) )?.map((element, index) => {
+                  return (
+                    <tr>
+                      <td className="col-1">{element.MachineNo} </td>
+                      <td className="col-2">{element.ProductName} </td>
+                      <td className="col-3">{element.StartDate} </td>
+                      <td className="col-4">{element.PlannedQty} </td>
+                      <td className="col-5">{element.CurrentQty} </td>
+                      <td className="col-6">{element.Progress} </td>
+                      <td className="col-7">{element.Efficiency} </td>
+                      <td className="col-8">
+                        <Link to={`/dashboard/view/${element._id}`} className="arrow">{angles}</Link>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+              {/* <tr>
                 <td className="col-1">Sample </td>
                 <td className="col-2">Sample </td>
                 <td className="col-3">Sample </td>
@@ -78,16 +149,16 @@ const CurrentProducts = () => {
                   <Link
                     className="arrow"
                     to={"/dashboard/view/12544f"}
-                    onClick={(e) => togglePanel()}
+                   
                   >
                     {angles}
                   </Link>
                 </td>
-              </tr>
+              </tr> */}
             </table>
           </div>
 
-          <Outlet />
+
         </div>
       </div>
     </React.Fragment>
