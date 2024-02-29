71ff33c743dac5c3b7f170eace11dc94be734662
shehandezen <shehandezen@gmail.com>
2024-02-29 06:50:26 +0000

    implement mould changes UI

--- frontend/src/components/AddMouldChange.jsx
+++ frontend/src/components/AddMouldChange.jsx
@@ -1,17 +1,229 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFloppyDisk, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "../css/componentStyles/development.css";
+import "../css/componentStyles/addproduct.css";
+
 
 const AddMouldChange = () => {
+
+  const floppyDisk = <FontAwesomeIcon icon={faFloppyDisk} />;
+  const circleX = <FontAwesomeIcon icon={faXmark} />;
+  const navigate = useNavigate();
+
+  const [data, setData] = useState({
+    MachineNo: '',
+    Date: '',
+    PreviousProduct: '',
+    NextProduct: '',
+    PlannedTime: '',
+    Technician1: '',
+    Technician2: '',
+    ActualTime: '',
+    StartTime: '',
+    EndTime: '',
+    Note: ''
+
+  })
+
+
+  const handleChange = (value, type) => {
+    if (type == "machineNo") {
+      setData({ ...data, MachineNo: value });
+    } else if (type == "date") {
+      setData({ ...data, Date: value });
+    }else if (type == "previousproduct") {
+      setData({ ...data, PreviousProduct: value });
+    }
+    else if (type == "nextproduct") {
+      setData({ ...data, NextProduct: value });
+    }
+    else if (type == "plannedtime") {
+      setData({ ...data, PlannedTime: value });
+    }
+    else if (type == "technician1") {
+      setData({ ...data, Technician1: value });
+    }
+    else if (type == "technician2") {
+      setData({ ...data, Technician2: value });
+    }
+    else if (type == "actualtime") {
+      setData({ ...data, ActualTime: value });
+    }
+    else if (type == "starttime") {
+      setData({ ...data, StartTime: value });
+    }
+    else if (type == "endtime") {
+      setData({ ...data, EndTime: value });
+    }
+    else if (type == "note") {
+      setData({ ...data, Note: value });
+    }
+  }
+
+  const handleSubmit = (e) => {
+    const date = new Date('YYYY-MM-DD')
+    setData({...data, Date: date})
+    console.log(data);
+  };
+
   return (
-    <>
-      <div className="dev-container">
-        <img
-          src="https://x2yfn4-3000.csb.app/development.webp"
-          alt="developmet"
-        />
-        <div className="text">This Feature is under development.</div>
+    <React.Fragment>
+    <div className="add-product-container">
+      <div className="head">
+        Add New Production
+        <span className="line"></span>
       </div>
-    </>
+     
+      <div className="inputs-container">
+        <div className="input-container">
+          <div className="input-label"> Machine No </div>
+          <select
+            name="machine"
+            onChange={(e) => handleChange(e.target.value, "machineNo")}
+          >
+            <option value="">Machine No</option>
+            <option value="IM 01">IM 01</option>
+            <option value="IM 02">IM 02</option>
+            <option value="IM 03">IM 03</option>
+            <option value="IM 04">IM 04</option>
+            <option value="IM 05">IM 05</option>
+            <option value="IM 06">IM 06</option>
+            <option value="IM 07">IM 07</option>
+            <option value="IM 08">IM 08</option>
+            <option value="IM 09">IM 09</option>
+            <option value="IM 10">IM 10</option>
+            <option value="IM 11">IM 11</option>
+            <option value="IM 12">IM 12</option>
+            <option value="IM 13">IM 13</option>
+            <option value="IM 14">IM 14</option>
+            <option value="IM 15">IM 15</option>
+            <option value="IM 16">IM 16</option>
+            <option value="IM 17">IM 17</option>
+            <option value="IM 18">IM 18</option>
+            <option value="IM 19">IM 19</option>
+            <option value="IM 20">IM 20</option>
+            <option value="IM 21">IM 21</option>
+            <option value="IM 22">IM 22</option>
+            <option value="IM 23">IM 23</option>
+            <option value="IM 24">IM 24</option>
+            <option value="IML 01">IML 01</option>
+            <option value="IML 02">IML 02</option>
+            <option value="IML 03">IML 03</option>
+            <option value="IML 04">IML 04</option>
+            <option value="IML 05">IML 05</option>
+            <option value="IML 06">IML 06</option>
+            <option value="BM 01">BM 01</option>
+            <option value="BM 02">BM 02</option>
+            <option value="BM 03">BM 03</option>
+            <option value="BM 04">BM 04</option>
+            <option value="BM 05">BM 05</option>
+            <option value="BM 06">BM 06</option>
+            <option value="BM 07">BM 07</option>
+            <option value="BM 08">BM 08</option>
+            <option value="BM 09">BM 09</option>
+            <option value="BM 10">BM 10</option>
+            <option value="IBM 01">IBM 01</option>
+          </select>
+        </div>
+        <div className="input-container">
+          <div className="input-label">  Previous Product Name </div>
+          <select
+            name="previousproduct"
+            onChange={(e) => handleChange(e.target.value, "previousproduct")}
+          >
+             <option value="">Previous Product Name</option>
+             <option value="IBM 01">IBM 01</option>
+          </select>
+        </div>
+        <div className="input-container">
+          <div className="input-label"> Next Product Name </div>
+          <select
+            name="nextproduct"
+            onChange={(e) => handleChange(e.target.value, "nextproduct")}
+          >
+             <option value="">Next Product Name</option>
+             <option value="IBM 01">IBM 01</option>
+          </select>
+        </div>
+       
+        <div className="input-container">
+          <div className="input-label"> Planning Time </div>
+          <input
+            type="number"
+            placeholder="Planning time "
+            onChange={(e) => handleChange(e.target.value, "plannedtime")}
+          />
+        </div>
+        <div className="input-container">
+          <div className="input-label"> Actual Time </div>
+          <input
+            type="number"
+            placeholder="Actual time "
+            onChange={(e) => handleChange(e.target.value, "actualtime")}
+          />
+        </div>
+        <div className="input-container">
+          <div className="input-label"> Start Time </div>
+          <input
+            type="time"
+            placeholder="Start time"
+            onChange={(e) => handleChange(e.target.value, "starttime")}
+          />
+        </div>
+        <div className="input-container">
+          <div className="input-label"> End Time </div>
+          <input
+            type="time"
+            placeholder="End Time"
+            onChange={(e) => handleChange(e.target.value, "endtime")}
+          />
+        </div>
+        <div className="input-container">
+          <div className="input-label"> Technician </div>
+          <input
+            type="text"
+            placeholder="Technician"
+            onChange={(e) => handleChange(e.target.value, "technician1")}
+          />
+        </div>
+        <div className="input-container">
+          <div className="input-label"> Technician </div>
+          <input
+            type="text"
+            placeholder="Technician"
+            onChange={(e) =>
+              handleChange(e.target.value, "technician2")
+            }
+          />
+        </div>
+        <div className="input-container">
+          <div className="input-label"> Note </div>
+          <input
+            type="text"
+            placeholder="Note"
+            onChange={(e) => handleChange(e.target.value, "note")}
+          />
+        </div>
+        </div>
+     
+      <div className=" buttons">
+        <button onClick={(e) => handleSubmit(e)}>Save {floppyDisk}</button>
+
+        <button
+          style={{ background: "var(--red)" }}
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          {" "}
+          {circleX} Discard
+        </button>
+      </div>
+    </div>
+  </React.Fragment>
+
   );
 };
 
--- frontend/src/components/CurrentMouldChange.jsx
+++ frontend/src/components/CurrentMouldChange.jsx
@@ -1,19 +1,121 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import "../css/componentStyles/development.css";
+import React, { useRef, useState, useEffect } from "react";
+import { Outlet, Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+  faAngleRight,
+  faAnglesRight,
+} from "@fortawesome/free-solid-svg-icons";
+import "../css/componentStyles/detailview.css";
 
 const CurrentMouldChange = () => {
+
+  const search = <FontAwesomeIcon icon={faSearch} />;
+  // const angle = <FontAwesomeIcon icon={faAngleRight} />;
+  const angles = <FontAwesomeIcon icon={faAnglesRight} />;
+
+  const [searchText, setSearchText] = useState('')
+
+  const searchHandle = (e) => {
+    setSearchText(e.target.value)
+    console.log(e.target.value)
+    console.log(data.filter(obj => obj.MachineNo.includes(searchText) ))
+  }
+  const [data, setDate] = useState([{
+    _id: '154gfew65g1ew6v4',
+    MachineNo: 'Im 01',
+    PreviousProduct: '4L lid',
+    NextProduct: '1L Can lid',
+    PlannedTime: '60 Min.',
+    Technicians: 'Technician 1, Technician 2'
+
+  },
+  {
+    _id: '154gfew65g1ew6v4',
+    MachineNo: 'Im 01',
+    PreviousProduct: '4L lid',
+    NextProduct: '1L Can lid',
+    PlannedTime: '60 Min.',
+    Technicians: 'Technician 1, Technician 2'
+    
+  },
+  {
+    _id: '154gfew65g1ew6v4',
+    MachineNo: 'Im 01',
+    PreviousProduct: '4L lid',
+    NextProduct: '1L Can lid',
+    PlannedTime: '60 Min.',
+    Technicians: 'Technician 1, Technician 2'
+    
+  },
+  {
+    _id: '154gfew65g1ew6v4',
+    MachineNo: 'Im 01',
+    PreviousProduct: '4L lid',
+    NextProduct: '1L Can lid',
+    PlannedTime: '60 Min.',
+    Technicians: 'Technician 1, Technician 2'
+    
+  },
+  {
+    _id: '154gfew65g1ew6v4',
+    MachineNo: 'Im 01',
+    PreviousProduct: '4L lid',
+    NextProduct: '1L Can lid',
+    PlannedTime: '60 Min.',
+    Technicians: 'Technician 1, Technician 2'
+    
+  },
+  
+  ])
+
+
   return (
-    <>
-      <div className="dev-container">
-        <img
-          src="https://x2yfn4-3000.csb.app/development.webp"
-          alt="developmet"
-        />
-        <div className="text">This Feature is under development.</div>
+    <React.Fragment>
+    <div className="details-container">
+      <div className="top">
+        <div className="search">
+          <input type="text" placeholder="Search Mould Changes" onChange={(e) => { searchHandle(e) }} />
+          <div className="search-icon"> {search} </div>
+        </div>
       </div>
-      <Outlet />
-    </>
+      <div className="wrapper">
+        <div className="table-container" >
+          <table>
+            <tr>
+              <th className="col-1">Machine No</th>
+              <th className="col-2">Previous Product</th>
+              <th className="col-3">Next Product</th>
+              <th className="col-4">Technicians</th>
+              <th className="col-5">Planned Time</th>
+              <th className="col-8">{angles}</th>
+            </tr>
+
+            {
+              data?.filter(obj => obj?.MachineNo?.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()) )?.map((element, index) => {
+                return (
+                  <tr>
+                    <td className="col-1">{element.MachineNo} </td>
+                    <td className="col-2">{element.PreviousProduct} </td>
+                    <td className="col-3">{element.NextProduct} </td>
+                    <td className="col-4">{element.PlannedTime} </td>
+                    <td className="col-5">{element.Technicians} </td>
+                    
+                    <td className="col-8">
+                      <Link to={`/dashboard/view/${element._id}`} className="arrow">{angles}</Link>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+           
+          </table>
+        </div>
+
+
+      </div>
+    </div>
+  </React.Fragment>
   );
 };
 
